0aacbee00020c40de7864ce1c8f65e2b394db0a0
Symmetree <symmetree@163.com>
2025-05-31 01:29:30 +0800

    add hw10

---
+++ mips64/a.s
@@ -0,0 +1,57 @@
+	.file	1 "a.c"
+	.section .mdebug.abi64
+	.previous
+	.nan	legacy
+	.module	fp=64
+	.module	oddspreg
+	.module	arch=mips64r2
+	.abicalls
+	.text
+	.rdata
+	.align	3
+.LC0:
+	.ascii	"Hello,MIPS64!\000"
+	.text
+	.align	2
+	.globl	main
+	.set	nomips16
+	.set	nomicromips
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$fp,32,$31		# vars= 0, regs= 3/0, args= 0, gp= 0
+	.mask	0xd0000000,-8
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	daddiu	$sp,$sp,-32
+	sd	$31,24($sp)
+	sd	$fp,16($sp)
+	sd	$28,8($sp)
+	move	$fp,$sp
+	lui	$28,%hi(%neg(%gp_rel(main)))
+	daddu	$28,$28,$25
+	daddiu	$28,$28,%lo(%neg(%gp_rel(main)))
+	ld	$2,%got_page(.LC0)($28)
+	daddiu	$4,$2,%got_ofst(.LC0)
+	ld	$2,%call16(puts)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,puts
+1:	jalr	$25
+	nop
+
+	move	$2,$0
+	move	$sp,$fp
+	ld	$31,24($sp)
+	ld	$fp,16($sp)
+	ld	$28,8($sp)
+	daddiu	$sp,$sp,32
+	jr	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 12.3.0-17ubuntu1) 12.3.0"
+	.section	.note.GNU-stack,"",@progbits
